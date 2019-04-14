c4d1eeb64f5cb412541cf671dc014743c677451e
Jonny Edge <jonnyedge@outlook.com>
2019-04-14 16:27:06 +0100

    Styled horizontal nav, added basic H2 header.

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import '../styles/app.scss';
 import Nav from '../components/nav';
+import Header from '../components/header';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="nav">
+      <div className="app">
         <Nav />
+        <Header />
       </div>
     );
   }
---
+++ src/components/header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/header.scss';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className="header">
+        <h2>Talk amongst yourselves</h2>
+      </div>
+    );
+  }
+}
+
+export default Header;
